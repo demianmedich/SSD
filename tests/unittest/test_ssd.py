10cd62911fd4e44cfda398b823028624052080e5
Jungsoo Kim <jungsooo.kim@samsung.com>
2024-06-18 11:39:11 +0900

    test ssd

--- tests/unittest/test_ssd.py
+++ tests/unittest/test_ssd.py
@@ -1,9 +1,16 @@
+import os
 import unittest
 
 
 class SSDTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)  # add assertion here
+
+    def test_is_exist_nand_txt(self):
+        if not os.path.exists("nand.txt"):
+            with self.assertRaises(Exception):
+                with open("nand.txt", "r") as f:
+                    pass
 
 
 if __name__ == "__main__":
