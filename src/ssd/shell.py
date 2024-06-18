694a07c33af945aada2acbade2501691dcdf08cc
daeha.ryu <daeha.ryu@samsungds.net>
2024-06-18 13:48:15 +0900

    shell class name make

--- src/ssd/shell.py
+++ src/ssd/shell.py
@@ -1,1 +1,19 @@
 # coding=utf-8
+class Shell:
+    def write(self):
+        pass
+
+    def read(self):
+        pass
+
+    def exit(self):
+        pass
+
+    def help(self):
+        pass
+
+    def full_write(self):
+        pass
+
+    def full_read(self):
+        pass
