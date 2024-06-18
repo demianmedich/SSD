61a01ada5c12f67c326efdc04a279e51a7b19b79
BR-Lab <93640309+BR-Lab@users.noreply.github.com>
2024-06-18 15:57:54 +0900

    Feature/shell/write&exit (#9)
    * first commit
    
    * [feature] add shell write ft
    
    * [refactoring] add magic number
    
    * [feature] add shell exit ft
    
    * [feature] add validation ft
    
    * [feature] shell write&exit 함수 수정
    ssd 함수 call -> os.system 명령어로 변경
    
    * [feature] shell write 함수 validation에 '0x'로 시작하는지에 대한 조건 추가
    
    * [feature] cmd에서 ssd 부르는 스크립트 수정
    ssd -> core.py
    
    ---------
    
    Co-authored-by: bora <bora.jeong@samsung.com>

--- tests/unittest/test_shell.py
+++ tests/unittest/test_shell.py
@@ -1,9 +1,26 @@
 import unittest
+from unittest.mock import Mock
+
+from src.ssd.shell import Shell
+
+ADDRESS = 3
+VALUE = "0xAAAABBBB"
 
 
-class SSDShellTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+class ShellTestCase(unittest.TestCase):
+    def setUp(self):
+        self.mk = Mock()
+        self.sut = Shell(self.mk)
+
+    def test_shell_write_call_SSD_write(self):
+        self.assertIsNone(self.sut.write(ADDRESS, VALUE))
+
+    def test_shell_exit(self):
+        self.assertIsNone(self.sut.exit())
+
+
+def test_shell_exit(self):
+    pass
 
 
 if __name__ == "__main__":
