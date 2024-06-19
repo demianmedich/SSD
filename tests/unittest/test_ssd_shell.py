d7810f46fc535b837b4d9b6caa26fd15b6e87b75
BR-Lab <93640309+BR-Lab@users.noreply.github.com>
2024-06-19 11:03:04 +0900

    Update test_ssd_shell.py

--- tests/unittest/test_ssd_shell.py
+++ tests/unittest/test_ssd_shell.py
@@ -11,58 +11,47 @@ class TestSsdShell(unittest.TestCase):
         self.sut = SsdShell(self.mk)
 
     def test_write_one_arg(self):
-        with patch("sys.stdout", new=StringIO()) as mock_out:
-            self.sut.onecmd("write 1")
-            self.mk.help.assert_called()
+        self.sut.onecmd("write 1")
+        self.mk.help.assert_called()
 
     def test_write_none_hex_arg(self):
-        with patch("sys.stdout", new=StringIO()) as mock_out:
-            self.sut.onecmd("write 10 0x1G2HAAAA")
-            self.mk.help.assert_called()
+        self.sut.onecmd("write 10 0x1G2HAAAA")
+        self.mk.help.assert_called()
 
     def test_write(self):
-        with patch("sys.stdout", new=StringIO()) as mock_out:
-            self.sut.onecmd("write 10 0xAAAABBBB")
-            self.mk.write.assert_called()
+        self.sut.onecmd("write 10 0xAAAABBBB")
+        self.mk.write.assert_called()
 
     def test_read_invalid_address(self):
-        with patch("sys.stdout", new=StringIO()) as mock_out:
-            self.sut.onecmd("read 1000")
-            self.mk.help.assert_called()
+        self.sut.onecmd("read 1000")
+        self.mk.help.assert_called()
 
     def test_read(self):
-        with patch("sys.stdout", new=StringIO()) as mock_out:
-            self.sut.onecmd("read 10")
-            self.mk.read.assert_called()
+        self.sut.onecmd("read 10")
+        self.mk.read.assert_called()
 
     def test_exit(self):
-        with patch("sys.stdout", new=StringIO()) as mock_out:
-            self.assertTrue(self.sut.onecmd("exit"))
+        self.assertTrue(self.sut.onecmd("exit"))
 
     def test_help(self):
-        with patch("sys.stdout", new=StringIO()) as mock_out:
-            self.sut.onecmd("help")
-            self.mk.help.assert_called()
+        self.sut.onecmd("help")
+        self.mk.help.assert_called()
 
     def test_full_write(self):
-        with patch("sys.stdout", new=StringIO()) as mock_out:
-            self.sut.onecmd("fullwrite")
-            self.mk.fullwrite.assert_called()
+        self.sut.onecmd("fullwrite")
+        self.mk.fullwrite.assert_called()
 
     def test_full_read(self):
-        with patch("sys.stdout", new=StringIO()) as mock_out:
-            self.sut.onecmd("fullread")
-            self.mk.fullread.assert_called()
+        self.sut.onecmd("fullread")
+        self.mk.fullread.assert_called()
 
     def test_testapp1(self):
-        with patch("sys.stdout", new=StringIO()) as mock_out:
-            self.sut.onecmd("testapp1")
-            self.mk.testapp1.assert_called()
+        self.sut.onecmd("testapp1")
+        self.mk.testapp1.assert_called()
 
     def test_testapp2(self):
-        with patch("sys.stdout", new=StringIO()) as mock_out:
-            self.sut.onecmd("testapp2")
-            self.mk.testapp2.assert_called()
+        self.sut.onecmd("testapp2")
+        self.mk.testapp2.assert_called()
 
     def test_wrong_cmd(self):
         with patch("sys.stdout", new=StringIO()) as mock_out:
