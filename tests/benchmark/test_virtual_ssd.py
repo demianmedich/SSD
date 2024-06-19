af1ce702fd2972f46236d5deaefb9dfe419d88a9
demianmedich <demianmedich@gmail.com>
2024-06-19 11:41:46 +0900

    Added benchmark tests

---
+++ tests/benchmark/test_virtual_ssd.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+import cProfile
+import tempfile
+from itertools import cycle, islice
+from unittest import TestCase
+
+from ssd.core.impl import VirtualSSD
+
+
+class VirtualSSDTestCase(TestCase):
+
+    def test_read_performance(self):
+        ssd = VirtualSSD()
+        loop = 100000
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            ssd.set_rootdir(tmpdir)
+
+            with cProfile.Profile() as profiler:
+                for i in islice(cycle(range(100)), loop):
+                    ssd.read(i)
+                profiler.print_stats(sort="cumulative")
+
+    def test_write_performance(self):
+        ssd = VirtualSSD()
+        loop = 100000
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            ssd.set_rootdir(tmpdir)
+
+            with cProfile.Profile() as profiler:
+                for i in islice(cycle(range(100)), loop):
+                    ssd.write(i, i)
+                profiler.print_stats(sort="cumulative")
