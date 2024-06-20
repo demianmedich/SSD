23a04ade30cce8c6ad329e7886ca703a7dbf304a
daeha.ryu <daeha.ryu@samsungds.net>
2024-06-20 10:39:37 +0900

    [feature] update function name style for ()

--- src/ssd/core/logger.py
+++ src/ssd/core/logger.py
@@ -1,5 +1,6 @@
 import datetime
 import inspect
+import os
 
 
 class Logger:
@@ -8,21 +9,36 @@ class Logger:
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
             cls._instance = super().__new__(cls, *args, **kwargs)
+            root_folder = cls.find_git_root()
+            log_folder = os.path.join(root_folder, "log")
+            os.makedirs(log_folder, exist_ok=True)
         return cls._instance
 
+    @staticmethod
+    def find_git_root():
+        current_dir = os.getcwd()
+        while not os.path.exists(os.path.join(current_dir, ".git")):
+            current_dir = os.path.dirname(current_dir)
+            if current_dir == os.path.dirname(current_dir):
+                raise RuntimeError("Git root not found.")
+        return current_dir
+
     def save_log(self, log):
-        with open("latest.log", "a") as file:
+        root_folder = self.find_git_root()
+        log_folder = os.path.join(root_folder, "log")
+        log_file_path = os.path.join(log_folder, "latest.log")
+        with open(log_file_path, "a") as file:
             file.write(log + "\n")
+            print(log)
 
     def print(self, message):
         current_time = datetime.datetime.now().strftime("[%Y.%m.%d %H:%M:%S]")
         len_function = 30
         call = inspect.stack()[1]
         function_name = call.function
-        function_name = function_name[:len_function]
-        log = f"{current_time} {function_name.center(len_function)+'()'} : {message}"
+        function_name = function_name[:len_function] + "()"
+        log = f"{current_time} {function_name.center(len_function)} : {message}"
         self.save_log(log)
-        print(log)
 
 
 def test_qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq():
@@ -34,4 +50,4 @@ def test_qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq(
 
 
 if __name__ == "__main__":
-    test_qqq()
+    test_qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq()
