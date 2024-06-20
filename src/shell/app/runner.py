38042aa64e5cdf68068fd2dca2c864ba9072704c
ihansam <52237832+ihansam@users.noreply.github.com>
2024-06-20 13:52:33 +0900

    Feature: Runner 구조 (#54)
    * add Shell Main Interface
    
    * add Apps
    
    * implement 간단하게 runner app
    
    * change filenames
    
    * add 더 명확한 에러 처리
    
    * refactor: Shell에서도 ScriptManager를 사용해 동적으로 script를 찾아 실행하기 위함

---
+++ src/shell/app/runner.py
@@ -0,0 +1,42 @@
+"""
+TODO:
+    모든 검증 스크립트는 성공 실패 여부를 리턴해야 한다.
+    그 검증 스크립트는 Shell에서도 실행할 수 있어야 한다.
+"""
+
+from pathlib import Path
+
+from shell.app.util import ScriptManager
+
+
+class SsdTestRunnerApp:
+    def __init__(self):
+        self._script_manager = ScriptManager()
+
+    def execute_runlist(self, run_list_path: Path | str) -> None:
+        run_list_path = Path(run_list_path).absolute()
+        if not run_list_path.exists():
+            print("FileNotFoundError: Run list file does not exist")
+            return
+
+        script_names = self._parse_run_list(run_list_path)
+
+        try:
+            script_paths = self._script_manager.collect(script_names)
+            print(script_paths)
+        except FileNotFoundError as e:
+            print(f"FileNotFoundError: {e}")
+            return
+
+        for _name, _path in zip(script_names, script_paths):
+            print(f"{_name}\t---\tRun...", end="")
+            test_result = self._script_manager.execute(_path)
+            if not test_result:
+                print("FAIL!")
+                break
+            print("Pass")
+
+    @staticmethod
+    def _parse_run_list(run_list: Path) -> list[str]:
+        with open(run_list, "r") as file:
+            return file.read().strip().split("\n")
