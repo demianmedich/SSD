7be48dc036f0e3607b4aa6e17474439c10241182
Kidan Jin <ihansam0322@gmail.com>
2024-06-20 14:39:49 +0900

    implement test scripts

--- src/script/testapp2.py
+++ src/script/testapp2.py
@@ -1,1 +1,25 @@
-print("TESTAPP2!")
+import os
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+from ssd.shell import ReadResultAccessor, Shell  # noqa
+
+api = Shell(ReadResultAccessor(Path(os.getcwd())))
+
+test_value1 = "0xAAAABBBB"
+test_value2 = "0x12345678"
+target_lba = list(range(0, 6))
+
+for _ in range(30):
+    for lba in target_lba:
+        api.write(lba, test_value1)
+
+for lba in target_lba:
+    api.write(lba, test_value2)
+
+for lba in target_lba:
+    if api.read(lba) != test_value2:
+        sys.exit(1)
+
+sys.exit(0)
