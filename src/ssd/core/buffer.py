9784b022c541d2868865fa062907ca29cd709376
Jungsoo Kim <jungsooo.kim@samsung.com>
2024-06-20 15:07:52 +0900

    fix buffer.txt 생성 case

--- src/ssd/core/buffer.py
+++ src/ssd/core/buffer.py
@@ -13,7 +13,8 @@ class CommandBuffer:
         self._buffer_txt_path = rootdir / BUFFER_TXT
         self._ssd = ssd
 
-        self._make_initial_buffer()
+        if not self._buffer_txt_path.exists():
+            self._make_initial_buffer()
 
     def flush(self) -> None:
         cmds = self._read_commands_buffer_txt()
@@ -25,6 +26,7 @@ class CommandBuffer:
                     self._ssd.erase(addr, size=value_or_cnt)
                 # case _:
                 #     raise ValueError(f"Invalid opcode {opcode}")
+        self._make_initial_buffer()
 
     def read(self, requested_address: int) -> str:
         if requested_address < 0 or requested_address >= 100:
@@ -69,7 +71,7 @@ class CommandBuffer:
         # return cmds
 
     def _make_initial_buffer(self):
-        with open(self._buffer_txt_path, mode="a+", encoding="utf-8", newline="\n"):
+        with open(self._buffer_txt_path, mode="w", encoding="utf-8", newline="\n"):
             pass
 
     def _optimize_commands(self, commands: list[str]) -> bool:
