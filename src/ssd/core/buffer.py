d83a87e239b397445e347a9be2f36e5cc4d9c050
bora <bora.jeong@samsung.com>
2024-06-20 16:54:08 +0900

    [refactoring] optimize command 함수 에서 method 추출

--- src/ssd/core/buffer.py
+++ src/ssd/core/buffer.py
@@ -80,49 +80,16 @@ class CommandBuffer:
         i = len(commands) - 1
         while i > 0:
             changed = False
+
             j = 0
             while j < i:
                 i_updated = False
-                opcode, addr, value = commands[i].split()
-                addr, value = int(addr), int(value)
+                opcode = commands[i].split()[0]
                 if opcode == "W":
-                    if commands[j].startswith(f"W {addr}"):
-                        i_updated = True
-                        del commands[j]
-                    elif commands[j].startswith("E"):
-                        _, erase_addr, erase_size = commands[j].split()
-                        erase_addr, erase_size = int(erase_addr), int(erase_size)
-                        if erase_addr <= addr < erase_addr + erase_size:
-                            i_updated = True
-                            del commands[j]
-                            if erase_addr + erase_size != addr + 1:
-                                commands.insert(
-                                    j,
-                                    f"E {addr + 1} {erase_addr + erase_size - addr - 1}",
-                                )
-                            if addr != erase_addr:
-                                commands.insert(j, f"E {addr} {addr - erase_addr}")
+                    i_updated = self.optimize_write_command(commands, i, j)
                 else:
-                    if commands[j].startswith("W"):
-                        _, write_addr, _ = commands[j].split()
+                    i_updated = self.optimize_erase_command(commands, i, j)
 
-                        if addr <= write_addr < addr + value:
-                            i_updated = True
-                            del commands[j]
-
-                    elif commands[j].startswith("E"):
-                        _, erase_addr, erase_size = commands[j].split()
-                        erase_addr, erase_size = int(erase_addr), int(erase_size)
-                        if (
-                            erase_addr <= addr <= erase_addr + erase_size
-                            or erase_addr <= addr + value <= erase_addr + erase_size
-                        ):
-
-                            min_addr = min(addr, erase_addr)
-                            max_addr = min(addr + value, erase_addr + erase_size)
-                            if max_addr - min_addr <= 10:
-                                i_updated = True
-                                commands[i] = "E {min_addr} {max_addr - min_addr}"
                 if i_updated:
                     i = j
                     j = 0
@@ -137,3 +104,53 @@ class CommandBuffer:
                 i -= 1
 
         return updated_commands, commands
+
+    def optimize_erase_command(self, commands, i, j):
+        i_updated = False
+        opcode, addr, value = commands[i].split()
+        addr, value = int(addr), int(value)
+
+        if commands[j].startswith("W"):
+            _, write_addr, _ = commands[j].split()
+
+            if addr <= write_addr < addr + value:
+                i_updated = True
+                del commands[j]
+
+        elif commands[j].startswith("E"):
+            _, erase_addr, erase_size = commands[j].split()
+            erase_addr, erase_size = int(erase_addr), int(erase_size)
+            if (
+                erase_addr <= addr <= erase_addr + erase_size
+                or erase_addr <= addr + value <= erase_addr + erase_size
+            ):
+
+                min_addr = min(addr, erase_addr)
+                max_addr = min(addr + value, erase_addr + erase_size)
+                if max_addr - min_addr <= 10:
+                    i_updated = True
+                    commands[i] = "E {min_addr} {max_addr - min_addr}"
+        return i_updated
+
+    def optimize_write_command(self, commands, i, j):
+        opcode, addr, value = commands[i].split()
+        addr, value = int(addr), int(value)
+
+        i_updated = False
+        if commands[j].startswith(f"W {addr}"):
+            i_updated = True
+            del commands[j]
+        elif commands[j].startswith("E"):
+            _, erase_addr, erase_size = commands[j].split()
+            erase_addr, erase_size = int(erase_addr), int(erase_size)
+            if erase_addr <= addr < erase_addr + erase_size:
+                i_updated = True
+                del commands[j]
+                if erase_addr + erase_size != addr + 1:
+                    commands.insert(
+                        j,
+                        f"E {addr + 1} {erase_addr + erase_size - addr - 1}",
+                    )
+                if addr != erase_addr:
+                    commands.insert(j, f"E {addr} {addr - erase_addr}")
+        return i_updated
