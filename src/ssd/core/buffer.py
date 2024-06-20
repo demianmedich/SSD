3b7d87f815f2bf675e5408d50edb551effa1f36d
Jungsoo Kim <jungsooo.kim@samsung.com>
2024-06-20 14:57:27 +0900

    flush update

--- src/ssd/core/buffer.py
+++ src/ssd/core/buffer.py
@@ -20,13 +20,11 @@ class CommandBuffer:
         for opcode, addr, value_or_cnt in cmds:
             match opcode:
                 case "W":
-                    self._ssd.write(addr, value_or_cnt)
+                    self._ssd.write(addr, data=value_or_cnt)
                 case "E":
-                    # TODO: erase 추가 필요.
-                    # self._ssd.erase(addr, value_or_cnt)
-                    pass
-                case _:
-                    raise ValueError(f"Invalid opcode {opcode}")
+                    self._ssd.erase(addr, size=value_or_cnt)
+                # case _:
+                #     raise ValueError(f"Invalid opcode {opcode}")
 
     def read(self, requested_address: int) -> str:
         if requested_address < 0 or requested_address >= 100:
@@ -47,20 +45,23 @@ class CommandBuffer:
         raise ValueError(f"Not found data")
 
     def write(self, cmd: str) -> None:
-        commands = self._read_commands_buffer_txt()
-        commands.append(cmd)
-        changed = self._optimize_commands(commands)
+        cmds = self._read_commands_buffer_txt()
+        cmds.append(cmd)
+        changed = self._optimize_commands(cmds)
 
         if changed:
             with open(
                 self._buffer_txt_path, mode="wt", encoding="utf-8", newline="\n"
             ) as f:
-                f.writelines(f"{cmd}\n" for cmd in commands)
+                f.writelines(f"{cmd}\n" for cmd in cmds)
         else:
             with open(
                 self._buffer_txt_path, mode="a+", encoding="utf-8", newline="\n"
             ) as f:
                 f.write(f"{cmd}\n")
+
+        if len(cmds) > 9:
+            self.flush()
 
     def _read_commands_buffer_txt(self) -> list[str]:
         commands = self._buffer_txt_path.read_text(encoding="utf-8").split("\n")
