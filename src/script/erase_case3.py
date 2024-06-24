fcc37fdc2cd0f381995b2e0eb764b092223edd6b
ryudaeha <52273956+ryudaeha@users.noreply.github.com>
2024-06-24 15:13:51 +0900

    Test/scenario (#65)
    * [feature] full write and erase test case added
    
    * [feature] excepttion erase test case
    
    * [feature] flush test case 1
    
    * [feature] add flush test case 2
    
    * [feature] erase test case 3
    
    * [feature] erase write multi test ccase
    
    ---------
    
    Co-authored-by: daeha.ryu <daeha.ryu@samsungds.net>

---
+++ src/script/erase_case3.py
@@ -0,0 +1,42 @@
+import os
+import sys
+from pathlib import Path
+
+from ssd.shell.api import ResultReader, Shell
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+api = Shell(ResultReader(Path(os.getcwd())))
+default_value = "0x00000000"
+test_value = "0x1234567A"
+api.fullwrite(test_value)
+data = api.fullread()
+
+for read_value in api.fullread():
+    if read_value != test_value:
+        sys.exit(1)
+
+for lba in range(0, 20):
+    api.erase(lba, 2)
+
+for lba in range(0, 20):
+    data = api.read(lba)
+    if data != default_value:
+        sys.exit(1)
+
+api.fullwrite(test_value)
+data = api.fullread()
+
+for read_value in api.fullread():
+    if read_value != test_value:
+        sys.exit(1)
+
+for lba in range(0, 20):
+    api.erase(lba, 9)
+
+for lba in range(0, 29):
+    data = api.read(lba)
+    if data != default_value:
+        sys.exit(1)
+
+sys.exit(0)
---
+++ src/script/flush_case1.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from pathlib import Path
+
+from ssd.shell.api import ResultReader, Shell
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+api = Shell(ResultReader(Path(os.getcwd())))
+default_value = "0x00000000"
+test_value = "0x1234567A"
+api.fullwrite(test_value)
+data = api.fullread()
+
+for read_value in api.fullread():
+    if read_value != test_value:
+        sys.exit(1)
+
+lba = 10
+test_value = "0x12312312"
+api.write(lba, test_value)
+api.flush()
+data = api.read(lba)
+if data != test_value:
+    sys.exit(1)
+
+lba = 11
+test_value = "0x12312312"
+for i in range(20, 25):
+    api.write(i, test_value)
+api.flush()
+data = api.read(lba)
+if data != test_value:
+    sys.exit(1)
+
+api.flush()
+data = api.read(lba)
+if data != test_value:
+    sys.exit(1)
+
+sys.exit(0)
