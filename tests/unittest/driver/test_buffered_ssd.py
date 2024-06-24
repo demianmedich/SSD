ab1765d4edf2a03d16a428f6b057c408bc97041f
BR-Lab <93640309+BR-Lab@users.noreply.github.com>
2024-06-24 17:00:12 +0900

    Feature/buffer optimize command (#69)
    * [feauter] add get function name
    
    * [feauter] add length of function name under 30
    
    * [feature] modify function name style
    
    * [feature] add save log function
    
    * [feature] update function name style for ()
    
    * [feature] add check latest log size function
    
    * [feature] add save until.log file
    
    * [feature] add find second until.log file logic
    
    * [feature] add change second until.log to until.zip
    
    * [feature] complete logger function
    
    * [feature]optimize command 함수 개발 중간 commit
    
    * update erase func calls buffer
    
    * modify file open mode
    
    * flush update
    
    * commands -> cmds 통일
    
    * fix buffer.txt 생성 case
    
    * unpacking cmds
    
    * [feature]optimize command 함수 개발 중간 commit2
    
    * fix buffer.txt 생성 case
    
    * unpacking cmds
    
    * [feature]optimize command 함수 개발 중간 commit3
    
    * [feature]optimize command 함수 개발 중간 commit3
    
    * fix buffer.txt 생성 case
    
    * unpacking cmds
    
    * [feature]optimize command 함수 개발 중간 commit4
    
    * [feature]optimize command 함수 개발 중간 commit4
    
    * [refactoring] optimize command 함수 에서 method 추출
    
    * Create buffer.txt
    
    * initial algorithms
    
    * fixed write->erase condi
    
    * refactor-1
    
    * refactor-2
    
    * refactor-3
    
    * [merge] merge remote branch
    
    * refactor-5
    
    * [merge] merge remote branch
    
    * refactor-6
    
    * refactor-7
    
    * refactor-8
    
    * refactor-9
    
    * refactor-10
    
    * refactor-11
    
    * refactor-12
    
    * refactor-13
    
    * refactor-14
    
    * refactor-15
    
    * refactor-16
    
    * refactor-17
    
    * refactor-18
    
    * refactor-19
    
    * bugfix-1
    
    * bugfix-2
    
    * refactor-20
    
    * refactor-21
    
    * refactor-22
    
    * refactor-23
    
    * feature flush call cmds more than 10
    
    * feature flush call cmds more than 10
    
    * [feature] do_flash 기능 추가
    
    * [feature] do_flash가 ssd_ctrl 통해 작동하도록 수정
    
    * refactoring index
    
    * flush 제거
    
    * flush 제거
    
    * [test] buffered_ssd test 작성
    
    * [feature] erase에서 merge되면 size가 10 이상인 경우,
    시작 addr부터 size 10개의 erase 명령어와 그 다음 addr부터의 erase 명령어로 나누어 지도록 수정
    
    ---------
    
    Co-authored-by: daeha.ryu <daeha.ryu@samsungds.net>
    Co-authored-by: bora <bora.jeong@samsung.com>
    Co-authored-by: Jungsoo Kim <jungsooo.kim@samsung.com>
    Co-authored-by: Jungsoo-Kim <62647549+Jungsoo-Kim@users.noreply.github.com>

--- tests/unittest/driver/test_buffered_ssd.py
+++ tests/unittest/driver/test_buffered_ssd.py
@@ -12,6 +12,9 @@ class BufferedSSDTestCase(unittest.TestCase):
     def read_from_result_file(self, rootdir: Path) -> int:
         return int((rootdir / "result.txt").read_text(), 16)
 
+    def read_from_buffer_file(self, rootdir: Path):
+        return (rootdir / "buffer.txt").read_text().split("\n")
+
     def extract_nand_data(self, tmpdir: Path, addr: int):
         return (tmpdir / "nand.txt").read_text().split("\n")[addr].split("\t")[-1]
 
@@ -41,6 +44,162 @@ class BufferedSSDTestCase(unittest.TestCase):
             actual = self.extract_nand_data(tmpdir, addr)
 
             self.assertEqual(expected, int(actual, 16))
+
+    def test_overwrite(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tmpdir = Path(tmpdir)
+            ssd = CommandBufferedSSD(
+                ErasableVirtualSSD(VirtualSSD(rootdir=tmpdir)), rootdir=tmpdir
+            )
+
+            addr = 0
+            value = 0x11111111
+            ssd.write(addr, value)
+            expected = 0xAAAABBBB
+            ssd.write(addr, expected)
+            ssd.read(addr)
+
+            self.assertEqual(expected, self.read_from_result_file(tmpdir))
+            self.assert_all_nand_data_zero(tmpdir)
+
+            ssd.flush()
+            actual = self.extract_nand_data(tmpdir, addr)
+
+            self.assertEqual(expected, int(actual, 16))
+
+    def test_split_erase(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tmpdir = Path(tmpdir)
+            ssd = CommandBufferedSSD(
+                ErasableVirtualSSD(VirtualSSD(rootdir=tmpdir)), rootdir=tmpdir
+            )
+
+            erase_addr = 0
+            size = 5
+            ssd.erase(erase_addr, size)
+
+            addr = 3
+            value = 0x11111111
+            ssd.write(addr, value)
+
+            expected = ["E 0 3", "E 4 1", "W 3 0x11111111", ""]
+
+            self.assertEqual(expected, self.read_from_buffer_file(tmpdir))
+            self.assert_all_nand_data_zero(tmpdir)
+
+    def test_merge_erase(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tmpdir = Path(tmpdir)
+            ssd = CommandBufferedSSD(
+                ErasableVirtualSSD(VirtualSSD(rootdir=tmpdir)), rootdir=tmpdir
+            )
+
+            erase_addr = 0
+            size = 5
+            ssd.erase(erase_addr, size)
+
+            erase_addr = 5
+            size = 1
+            ssd.erase(erase_addr, size)
+
+            expected = ["E 0 6", ""]
+
+            self.assertEqual(expected, self.read_from_buffer_file(tmpdir))
+            self.assert_all_nand_data_zero(tmpdir)
+
+    def test_merge_erase_over_size(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tmpdir = Path(tmpdir)
+            ssd = CommandBufferedSSD(
+                ErasableVirtualSSD(VirtualSSD(rootdir=tmpdir)), rootdir=tmpdir
+            )
+
+            erase_addr = 0
+            size = 5
+            ssd.erase(erase_addr, size)
+
+            erase_addr = 5
+            size = 7
+            ssd.erase(erase_addr, size)
+
+            expected = ["E 0 10", "E 10 2", ""]
+
+            self.assertEqual(expected, self.read_from_buffer_file(tmpdir))
+            self.assert_all_nand_data_zero(tmpdir)
+
+    def test_erase(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tmpdir = Path(tmpdir)
+            ssd = CommandBufferedSSD(
+                ErasableVirtualSSD(VirtualSSD(rootdir=tmpdir)), rootdir=tmpdir
+            )
+
+            addr = 3
+            value = 0x11111111
+            ssd.write(addr, value)
+
+            erase_addr = 0
+            size = 5
+            ssd.erase(erase_addr, size)
+
+            expected = ["E 0 5", ""]
+            self.assertEqual(expected, self.read_from_buffer_file(tmpdir))
+
+            ssd.flush()
+            actual = self.extract_nand_data(tmpdir, addr)
+            self.assertEqual(0x00000000, int(actual, 16))
+
+    def test_flush(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tmpdir = Path(tmpdir)
+            ssd = CommandBufferedSSD(
+                ErasableVirtualSSD(VirtualSSD(rootdir=tmpdir)), rootdir=tmpdir
+            )
+
+            erase_addr = 0
+            size = 5
+            ssd.erase(erase_addr, size)
+
+            addr = 3
+            value = 0x11111111
+            ssd.write(addr, value)
+
+            addr = 0
+            value = 0x11111111
+            ssd.write(addr, value)
+
+            addr = 10
+            value = 0x11111111
+            ssd.write(addr, value)
+
+            erase_addr = 20
+            size = 5
+            ssd.erase(erase_addr, size)
+
+            addr = 30
+            value = 0x11111111
+            ssd.write(addr, value)
+
+            addr = 22
+            value = 0x11111111
+            ssd.write(addr, value)
+
+            addr = 99
+            value = 0x11111111
+            ssd.write(addr, value)
+
+            addr = 89
+            value = 0x11111111
+            ssd.write(addr, value)
+
+            self.assertEqual(1, len(self.read_from_buffer_file(tmpdir)))
+
+            for i in range(100):
+                actual = self.extract_nand_data(tmpdir, i)
+                if i in [3, 0, 10, 30, 22, 99]:
+                    self.assertEqual(value, int(actual, 16))
+                elif i in [1, 2, 4, 20, 21, 23, 24]:
+                    self.assertEqual(0x00000000, int(actual, 16))
 
 
 if __name__ == "__main__":
